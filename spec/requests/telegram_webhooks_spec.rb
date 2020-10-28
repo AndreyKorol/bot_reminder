1389d35ab4abc22aaa63c7cbbf4dee4c1f8e9688
Андрей Королько <andrey.korolko@mail.ru>
2020-10-28 20:40:07 +0300

    add spec for /event

--- spec/requests/telegram_webhooks_spec.rb
+++ spec/requests/telegram_webhooks_spec.rb
@@ -19,8 +19,26 @@ RSpec.describe TelegramWebhooksController, type: :telegram_bot_controller do
       expect(user.name).to eq('Andrey')
     end
   end
+
   describe '#event!' do
     subject { -> { dispatch_command :event } }
+    it do
+      should respond_with_message 'Type name for event.'
+
+      expect { dispatch_message('Event') }.to make_telegram_request(bot, :sendMessage)
+        .with(hash_including(text: 'Type date and time in format: dd.mm hh:mm. For example 09.11 13:15'))
+
+      expect { dispatch_message('09.11 13:15') }.to make_telegram_request(bot, :sendMessage)
+        .with(hash_including(text: 'Type the description of event.'))
+
+      expect { dispatch_message('My first event') }.to make_telegram_request(bot, :sendMessage)
+        .with(hash_including(text: 'Thanks, we will notify you one hour before the start'))
+
+      event = Event.first
+      expect(event.name).to eq(name)
+      expect(event.date).to eq(date)
+      expect(event.description).to eq(description)
+    end
   end
   describe '#events!' do
     subject { -> { dispatch_command :events } }
